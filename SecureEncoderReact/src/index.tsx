3071fcfc6d0f70aaf285622c3d559b21f1fb4e03
Kevin Chin <120100402+clchinkc@users.noreply.github.com>
2024-07-04 15:13:31 +0800

    chore: Update ReactDOM.createRoot to use type assertion

--- SecureEncoderReact/src/index.tsx
+++ SecureEncoderReact/src/index.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
 
-const root = ReactDOM.createRoot(document.getElementById("root")!)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
